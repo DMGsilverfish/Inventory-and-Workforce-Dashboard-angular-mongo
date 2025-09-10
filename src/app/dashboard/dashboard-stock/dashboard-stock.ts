568ff2103781a3a738031dd915663903660bf641
Daniel <danielgibson.pe@gmail.com>
2025-09-10 10:24:08 +0200

    components prep work

---
+++ src/app/dashboard/dashboard-stock/dashboard-stock.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-stock',
+  imports: [],
+  templateUrl: './dashboard-stock.html',
+  styleUrl: './dashboard-stock.css'
+})
+export class DashboardStock {
+
+}
