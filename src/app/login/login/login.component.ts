9aaa11e080c3d07b08aed6e2d725ea407230a74f
Daniel <danielgibson.pe@gmail.com>
2025-09-11 11:02:26 +0200

    login added

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  username: string = '';
+  password: string = '';
 
-  loginAsAdmin() {
-    this.router.navigate(['/admin']);
-  }
+  constructor(private router: Router, private http: HttpClient) {}
 
-  loginAsEmployee() {
-    this.router.navigate(['/employee']);
+  login() {
+    if (!this.username || !this.password) {
+      alert('Please enter a username and pin');
+      return;
+    }
+
+    this.http.get<any[]>('/admin.json').subscribe(admins => {
+      const admin = admins.find(a => a.name === this.username && a.pin === this.password);
+      if (admin) {
+        this.router.navigate(['./admin']);
+      } else {
+        this.http.get<any[]>('/employee.json').subscribe(employees => {
+          const employee = employees.find(e => e.name === this.username && e.pin === this.password);
+          if (employee) {
+            this.router.navigate(['./employee']);
+          } else {
+            alert('Invalid username or pin');
+          }
+        })
+      }
+    })
   }
 }
